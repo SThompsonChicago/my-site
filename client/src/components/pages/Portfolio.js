b93f96171bef0b1e3acd03be8149ad00e3e59ce9
Stephen Thompson <s31@umbc.edu>
2022-01-23 22:53:07 -0600

    updated portfolio

--- client/src/components/pages/Portfolio.js
+++ client/src/components/pages/Portfolio.js
@@ -5,91 +5,126 @@ export default function Portfolio() {
     return (
 <div className="card notification is-black is-size-6-mobile">
 
-            <div className="card-content notification is-black">
-            <p className="title is-4 is-size-6-mobile">
-                            Portfolio
-                        </p>
-
-
-    
-            </div>
-    <div className="columns">
-
-    <div className="column">
-<div className="card notification is-white">
-  <div className="card-image">
-    <figure className="image is-4by3">
-    <a href="https://sthompsonchicago.github.io/pandemic-simulator/">
-    <img src="https://sthompsonchicago.github.io/My_portfolio/assets/simulation.png" alt="Simulation image"/>
-    </a>
-    </figure>
-  </div>
-
-
-<div className="column">
-    <div className="content">
-    <p className="title is-4 is-size-6-mobile">Epidemic Simulator</p>
-    <p>This is is a React application that allows the user to view an animation of an epidemic. The user can input a variety of different parameters and see how the results change. 
+  <div className="card-content notification is-black">
+    <p className="title is-4 is-size-6-mobile">
+      Portfolio
     </p>
-    <ul>
+  </div>
 
-                                    <li><a href="https://sthompsonchicago.github.io/pandemic-simulator/">Link to deployed application.</a></li>
+  <div class="tile is-ancestor">
+  <div class="tile is-parent">
+    <article class="tile is-child box">
 
-                                    <li><a href="https://github.com/SThompsonChicago/pandemic-simulator">Link to GitHub repository.</a> </li>
-            
-                                  </ul>
-      <br/>
-    </div>
+          <div className="content">
+            <p className="title is-black is-4 is-size-6-mobile has-text-centered">MACROECONOMIC DYNAMICS</p>
+          </div>
+<div className="card notifcation is-black">
+    <div className="card-image">
+          <figure className="image is-4by3">
+            <a href="https://sthompsonchicago.github.io/macro-disequilibrium/">
+              <img src="https://sthompsonchicago.github.io/My_portfolio/assets/macrosimulation.png" alt="Simulation image"/>
+            </a>
+          </figure>
+        </div>
+</div>
+        <div className="column">
+          <div className="content">
+            <p>This is a Single Page Application created with React, which allows the user to choose inputs for a macroeconomic model and view an animation showing the results. The app solves the model using the Runge-Kutta method, and describes a disequilibrium framework in which users can understand the long-run dynamics with statistical calculations. 
+            </p>
+            <ul>
+              <li><a href="https://sthompsonchicago.github.io/macro-disequilibrium/">Link to deployed application.</a></li>
+              <li><a href="https://github.com/SThompsonChicago/macro-disequilibrium">Link to GitHub repository.</a> </li>
+            </ul>
+            <br/>
+          </div>
+        </div>
+    </article>
+  </div>
+  <div class="tile is-parent">
+    <article class="tile is-child box">
+    <div className="content">
+          <p className="title is-4 is-size-6-mobile has-text-centered is-underlined">MVC BLOG</p>
+        </div>
+
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <a href="https://still-refuge-81895.herokuapp.com/">
+              <img src="https://sthompsonchicago.github.io/My_portfolio/assets/appimage.png" alt="Image of blog app"/>
+            </a>
+          </figure>
+        </div>
+
+
+      <div className="column">
+        <div className="content">
+          <p>This is a full-stack web application created with the Model-View-Controller (MVC) architecture. It allows the user to create an account, write/edit/delete blog posts of their own, and also view posts by others. It was created using Express, Handlebars, MySQL, node and more.
+          </p>
+          <ul>
+            <li><a href="https://still-refuge-81895.herokuapp.com/">Link to deployed application.</a></li>
+            <li><a href="https://github.com/SThompsonChicago/tech-blog">Link to GitHub repository.</a> </li>
+          </ul>
+          <br/>
+        </div>
+      </div>
+
+    </article>
   </div>
 </div>
 
-
-</div>
-
-
-
-
-
+  
+  <div className="columns">
+    <div className="column">
+      <div className="card notification is-white">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <a href="https://sthompsonchicago.github.io/pandemic-simulator/">
+              <img src="https://sthompsonchicago.github.io/My_portfolio/assets/simulation.png" alt="Simulation image"/>
+            </a>
+          </figure>
+        </div>
 
         <div className="column">
-<div className="card notification is-white">
-  <div className="card-image">
-    <figure className="image is-4by3">
-    <a href="https://book-club-chicago.herokuapp.com">
-      <img src="https://sthompsonchicago.github.io/My_portfolio/assets/bookTalkPic.png" alt="Image of Book Talk app"/>
-    </a>
-    </figure>
-  </div>
+          <div className="content">
+            <p className="title is-4 is-size-6-mobile">Epidemic Simulator</p>
+            <p>This is is a React application that allows the user to view an animation of an epidemic. The user can input a variety of different parameters and see how the results change. 
+            </p>
+            <ul>
+              <li><a href="https://sthompsonchicago.github.io/pandemic-simulator/">Link to deployed application.</a></li>
+              <li><a href="https://github.com/SThompsonChicago/pandemic-simulator">Link to GitHub repository.</a> </li>
+            </ul>
+            <br/>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="column">
+      <div className="card notification is-white">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <a href="https://book-club-chicago.herokuapp.com">
+              <img src="https://sthompsonchicago.github.io/My_portfolio/assets/bookTalkPic.png" alt="Image of Book Talk app"/>
+            </a>
+          </figure>
+        </div>
 
 
-<div className="column">
-    <div className="content">
-    <p className="title is-4 is-size-6-mobile">Book Talk</p>
-    <p>This is a full stack web application that allows users upload and share book 
+      <div className="column">
+        <div className="content">
+          <p className="title is-4 is-size-6-mobile">Book Talk</p>
+          <p>This is a full stack web application that allows users upload and share book 
                               reviews with friends online. 
                               The aim is to make reading a more interactive and social experience.  
-    </p>
-    <ul>
-
-                                  <li><a href="https://book-club-chicago.herokuapp.com">Link to deployed application.</a></li>
-
-                                  <li><a href="https://github.com/SThompsonChicago/book-club">Link to GitHub repository.</a> </li>
-          
-                                </ul>
-      <br/>
+          </p>
+          <ul>
+            <li><a href="https://book-club-chicago.herokuapp.com">Link to deployed application.</a></li>
+            <li><a href="https://github.com/SThompsonChicago/book-club">Link to GitHub repository.</a> </li>
+          </ul>
+          <br/>
+        </div>
+      </div>
     </div>
   </div>
-
-
-
-
-</div>
-
-
-</div>
-
-
-
 </div>
 
 
