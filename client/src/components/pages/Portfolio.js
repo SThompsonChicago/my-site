d2b55d64a4066995e9b717ea1ca44518788eed32
Stephen Thompson <s31@umbc.edu>
2022-01-24 13:27:53 -0600

    re-format portfolio page

--- client/src/components/pages/Portfolio.js
+++ client/src/components/pages/Portfolio.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import Card from './Card'
 
+const styles = {
+  hov: {
+    cursor: 'pointer',
+  },
+  vertspace: {
+    marginTop:'25px',
+  },
+  space: {
+    margin:"5px",
+  },
+  right: {
+    float: "right",
+  },
+  noLineBlack: {
+    textDecoration: "none",
+    color: "black",
+  },
+  noLineWhite: {
+  textDecoration: "none",
+  color: "white",
+  }
+}
+
 export default function Portfolio() {
     return (
 <div className="card notification is-black is-size-6-mobile">
 
   <div className="card-content notification is-black">
     <p className="title is-4 is-size-6-mobile">
-      Portfolio
+      My React.js Single-Page Apps
     </p>
   </div>
 
@@ -16,13 +39,15 @@ export default function Portfolio() {
 
 
 
-  <div class="tile is-ancestor is-link">
+  <div class="tile is-ancestor">
   <div class="tile is-parent">
     <article class="tile is-child box">
-
+<div className="box notification is-black">
           <div className="content">
-            <p className="title is-black is-4 is-size-6-mobile has-text-centered">MACROECONOMIC DYNAMICS</p>
+            <p className="title is-black is-4 is-size-6-mobile has-text-centered">Macroeconomic Forecaster</p>
           </div>
+          </div>
+
 <div className="card notifcation is-black">
     <div className="card-image">
           <figure className="image is-4by3">
@@ -31,25 +56,190 @@ export default function Portfolio() {
             </a>
           </figure>
         </div>
-</div>
-        <div className="column">
+
+        </div>
+        <div  style={styles.vertspace} className="box">
           <div className="content">
-            <p>This is a Single Page Application created with React, which allows the user to choose inputs for a macroeconomic model and view an animation showing the results. The app solves the model using the Runge-Kutta method, and describes a disequilibrium framework in which users can understand the long-run dynamics with statistical calculations. 
+            <p>This React.js app allows the user to choose inputs for a macroeconomic model, and then view an animation showing the results. The app solves the model using the Runge-Kutta method, and describes a disequilibrium framework in which users can understand the long-run dynamics with statistical calculations. It is based on work in an <a href='https://onlinelibrary.wiley.com/doi/10.1111/meca.12377'>academic article</a> I published. 
             </p>
             <ul>
               <li><a href="https://sthompsonchicago.github.io/macro-disequilibrium/">Link to deployed application.</a></li>
               <li><a href="https://github.com/SThompsonChicago/macro-disequilibrium">Link to GitHub repository.</a> </li>
+              <li><a href="https://www.youtube.com/watch?v=fQPoRxZOOuA">Link to video demonstration.</a> </li>
+            </ul>
+            <br/>
+          </div>
+
+        </div>
+    </article>
+  </div>
+
+  <div class="tile is-parent">
+    <article class="tile is-child box">
+
+    <div className="box notification is-black">
+          <div className="content">
+            <p className="title is-black is-4 is-size-6-mobile has-text-centered">Epidemic Simulator</p>
+          </div>
+          </div>
+<div className="card notifcation is-black">
+    <div className="card-image">
+          <figure className="image is-4by3">
+          <a href="https://sthompsonchicago.github.io/pandemic-simulator/">
+              <img src="https://sthompsonchicago.github.io/My_portfolio/assets/simulation.png" alt="Simulation image"/>
+            </a>
+          </figure>
+        </div>
+</div>
+        <div  style={styles.vertspace} className="box">
+          <div className="content">
+          <p>This is a React application that allows the user to view an animation describing a simulation of an epidemiological model. The user can input a variety of different parameters and see how the results change. The model shows how public policy can help control the spread of diseases, and is based on a <a href='https://www.aimsciences.org/article/doi/10.3934/eect.2013.2.173'>paper</a> I published in graduate school.
+            </p>
+            <ul>
+              <li><a href="https://sthompsonchicago.github.io/pandemic-simulator/">Link to deployed application.</a></li>
+              <li><a href="https://github.com/SThompsonChicago/pandemic-simulator">Link to GitHub repository.</a> </li>
+              <li><a href="https://www.youtube.com/watch?v=zGMR58tAFeY">Link to video demonstration.</a> </li>
             </ul>
             <br/>
           </div>
         </div>
     </article>
   </div>
+</div>
+
+
+
+
+<div className="card-content notification is-black">
+    <p className="title is-4 is-size-6-mobile">
+      MERN Apps
+    </p>
+  </div>
+
+
+
+
+
+<div class="tile is-ancestor">
   <div class="tile is-parent">
     <article class="tile is-child box">
-    <div className="content">
-          <p className="title is-4 is-size-6-mobile has-text-centered is-underlined">MVC BLOG</p>
+
+    <div className="box notification is-black">
+          <div className="content">
+            <p className="title is-black is-4 is-size-6-mobile has-text-centered">MERN Blog</p>
+          </div>
+          </div>
+<div className="card notifcation is-black">
+    <div className="card-image">
+    <figure className="image is-4by3">
+    <a href="https://vast-mountain-43651.herokuapp.com/">
+      <img src="https://sthompsonchicago.github.io/My_portfolio/assets/Mernblog.png" alt="Image of blog app"/>
+    </a>
+    </figure>
         </div>
+</div>
+        <div  style={styles.vertspace} className="box">
+          <div className="content">
+          <p>This is a simple blog that allows a user to create an account, write blog posts and view posts by others. It is a full-stack application which was created with the MERN stack.
+            </p>
+            <ul>
+              <li><a href="https://vast-mountain-43651.herokuapp.com/">Link to deployed application.</a></li>
+              <li><a href="https://github.com/SThompsonChicago/mern-blog">Link to GitHub repository.</a> </li>
+            </ul>
+            <br/>
+          </div>
+        </div>
+    </article>
+  </div>
+  <div class="tile is-parent is-link">
+    <article class="tile is-child box">
+    <div className="box notification is-black">
+          <div className="content">
+            <p className="title is-black is-4 is-size-6-mobile has-text-centered">Shifty</p>
+          </div>
+          </div>
+        <div className="card notifcation is-black">
+        <div className="card-image">
+          <figure className="image is-4by3">
+          <a href="https://protected-bastion-67300.herokuapp.com/">
+              <img src="https://sthompsonchicago.github.io/My_portfolio/assets/shifty.png" alt="Image of shifty app"/>
+            </a>
+          </figure>
+        </div>
+        </div>
+
+
+        <div  style={styles.vertspace} className="box">
+        <div className="content">
+        <p>This is a MERN application created for service-industry professionals, especially bartenders. The application allows users to create accounts, view job postings and create posts for new jobs. This was a group project created with Git version control. My main contributions were in back-end development, using GraphQL and MongoDB. 
+          </p>
+          <ul>
+            <li><a href="https://protected-bastion-67300.herokuapp.com/">Link to deployed application.</a></li>
+            <li><a href="https://github.com/riveranelson99/shifty">Link to GitHub repository.</a> </li>
+          </ul>
+          <br/>
+        </div>
+      </div>
+
+    </article>
+  </div>
+</div>
+
+
+
+<div className="card-content notification is-black">
+    <p className="title is-4 is-size-6-mobile">
+      Apps with Relational Databases and Model-View-Controller Architecture
+    </p>
+  </div>
+
+
+
+<div class="tile is-ancestor">
+<div class="tile is-parent is-link">
+    <article class="tile is-child box">
+    <div className="box notification is-black">
+          <div className="content">
+            <p className="title is-black is-4 is-size-6-mobile has-text-centered">Book Talk</p>
+          </div>
+          </div>
+        <div className="card notifcation is-black">
+        <div className="card-image">
+          <figure className="image is-4by3">
+          <a href="https://book-club-chicago.herokuapp.com">
+              <img src="https://sthompsonchicago.github.io/My_portfolio/assets/bookTalkPic.png" alt="Image of Book Talk app"/>
+            </a>
+          </figure>
+        </div>
+        </div>
+
+
+        <div  style={styles.vertspace} className="box">
+        <div className="content">
+        <p>This is a full stack web application that allows users upload and share book 
+                              reviews with friends online. 
+                              The aim is to make reading a more interactive and social experience. This was a group project that I completed with Git version control. My main contribution was in creating the express routes and back-end structure, as well as helping to create front-end functionality using Handlebars.js.  
+          </p>
+          <ul>
+            <li><a href="https://book-club-chicago.herokuapp.com">Link to deployed application.</a></li>
+            <li><a href="https://github.com/SThompsonChicago/book-club">Link to GitHub repository.</a> </li>
+          </ul>
+          <br/>
+        </div>
+      </div>
+
+    </article>
+  </div>
+
+
+  
+  <div class="tile is-parent">
+    <article class="tile is-child box">
+    <div className="box notification is-black">
+          <div className="content">
+            <p className="title is-black is-4 is-size-6-mobile has-text-centered">Model-View-Controller Blog</p>
+          </div>
+          </div>
         <div className="card notifcation is-black">
         <div className="card-image">
           <figure className="image is-4by3">
@@ -61,9 +251,9 @@ export default function Portfolio() {
         </div>
 
 
-      <div className="column">
+        <div  style={styles.vertspace} className="box">
         <div className="content">
-          <p>This is a full-stack web application created with the Model-View-Controller (MVC) architecture. It allows the user to create an account, write/edit/delete blog posts of their own, and also view posts by others. It was created using Express, Handlebars, MySQL, node and more.
+          <p>This is a full-stack web application created with the Model-View-Controller (MVC) architecture. It allows the user to create an account, write/edit/delete blog posts of their own, and also view posts by others. It was created using Express, Handlebars, MySQL, and Node.
           </p>
           <ul>
             <li><a href="https://still-refuge-81895.herokuapp.com/">Link to deployed application.</a></li>
@@ -80,187 +270,6 @@ export default function Portfolio() {
 
 
 
-
-
-
-
-
-
-<div class="tile is-ancestor">
-  <div class="tile is-parent">
-    <article class="tile is-child box">
-
-          <div className="content">
-            <p className="title is-black is-4 is-size-6-mobile has-text-centered">EPIDEMIC SIMULATOR</p>
-          </div>
-<div className="card notifcation is-black">
-    <div className="card-image">
-          <figure className="image is-4by3">
-          <a href="https://sthompsonchicago.github.io/pandemic-simulator/">
-              <img src="https://sthompsonchicago.github.io/My_portfolio/assets/simulation.png" alt="Simulation image"/>
-            </a>
-          </figure>
-        </div>
-</div>
-        <div className="column">
-          <div className="content">
-          <p>This is is a React application that allows the user to view an animation of an epidemic. The user can input a variety of different parameters and see how the results change. 
-            </p>
-            <ul>
-              <li><a href="https://sthompsonchicago.github.io/pandemic-simulator/">Link to deployed application.</a></li>
-              <li><a href="https://github.com/SThompsonChicago/pandemic-simulator">Link to GitHub repository.</a> </li>
-            </ul>
-            <br/>
-          </div>
-        </div>
-    </article>
-  </div>
-  <div class="tile is-parent">
-    <article class="tile is-child box">
-    <div className="content">
-          <p className="title is-4 is-size-6-mobile has-text-centered is-underlined">BOOK TALK</p>
-        </div>
-        <div className="card notifcation is-black">
-        <div className="card-image">
-          <figure className="image is-4by3">
-          <a href="https://book-club-chicago.herokuapp.com">
-              <img src="https://sthompsonchicago.github.io/My_portfolio/assets/bookTalkPic.png" alt="Image of Book Talk app"/>
-            </a>
-          </figure>
-        </div>
-        </div>
-
-
-      <div className="column">
-        <div className="content">
-        <p>This is a full stack web application that allows users upload and share book 
-                              reviews with friends online. 
-                              The aim is to make reading a more interactive and social experience.  
-          </p>
-          <ul>
-            <li><a href="https://book-club-chicago.herokuapp.com">Link to deployed application.</a></li>
-            <li><a href="https://github.com/SThompsonChicago/book-club">Link to GitHub repository.</a> </li>
-          </ul>
-          <br/>
-        </div>
-      </div>
-
-    </article>
-  </div>
-</div>
-
-
-
-<div className="columns">
-
-  
-
-
-
-<div className="column">
-<div className="card notification is-white">
-  <div className="card-image">
-    <figure className="image is-4by3">
-    <a href="https://sthompsonchicago.github.io/Nonprofit-Career-Planner/">
-      <img src="https://sthompsonchicago.github.io/My_portfolio/assets/nonprofitPlanner.png" alt="Image of Nonprofit App"/>
-    </a>
-    </figure>
-  </div>
-
-
-<div className="column">
-    <div className="content">
-    <p className="title is-4 is-size-6-mobile">Career Planner</p>
-    <p>This web application helps the user plan a career in the nonprofit industry by looking up a list of nonprofits (and other relevant information) for a chosen city.
-    </p>
-    <ul>
-
-                                    <li><a href="https://sthompsonchicago.github.io/Nonprofit-Career-Planner/">Link to deployed application.</a></li>
-
-                                    <li><a href="https://github.com/SThompsonChicago/Nonprofit-Career-Planner">Link to GitHub repository.</a> </li>
-            
-                                  </ul>
-      <br/>
-    </div>
-  </div>
-</div>
-
-
-</div>
-
-
-
-
-
-
-
-
-
-
-<div className="column">
-<div className="card notification is-white">
-  <div className="card-image">
-    <figure className="image is-4by3">
-    <a href="https://vast-mountain-43651.herokuapp.com/">
-      <img src="https://sthompsonchicago.github.io/My_portfolio/assets/Mernblog.png" alt="Image of blog app"/>
-    </a>
-    </figure>
-  </div>
-
-
-<div className="column">
-    <div className="content">
-    <p className="title is-4 is-size-6-mobile">MERN Blog</p>
-    <p>This is a simple blog that allows a user to create an account, write blog posts and view posts by others. It is a full-stack application which was created with the MERN stack.
-    </p>
-    <ul>
-  
-                                <li><a href="https://vast-mountain-43651.herokuapp.com/">Link to deployed application.</a></li>
-
-                                <li><a href="https://github.com/SThompsonChicago/mern-blog">Link to GitHub repository.</a> </li>
-        
-                              </ul>
-      <br/>
-    </div>
-  </div>
-</div>
-
-
-
-{/* <div className="column">
-<div className="card notification is-white">
-  <div className="card-image">
-    <figure className="image is-4by3">
-    <a href="https://still-refuge-81895.herokuapp.com/">
-      <img src="https://sthompsonchicago.github.io/My_portfolio/assets/appimage.png" alt="Image of blog app"/>
-    </a>
-    </figure>
-  </div>
-
-
-<div className="column">
-    <div className="content">
-    <p className="title is-4 is-size-6-mobile">Blog</p>
-    <p>This is a simple blog that allows a user to create an account, write blog posts and view posts by others. It is a full-stack application using Express, Handlebars, MySQL, node and more.
-    </p>
-    <ul>
-  
-                                <li><a href="https://still-refuge-81895.herokuapp.com/">Link to deployed application.</a></li>
-
-                                <li><a href="https://github.com/SThompsonChicago/tech-blog">Link to GitHub repository.</a> </li>
-        
-                              </ul>
-      <br/>
-    </div>
-  </div>
-</div> */}
-
-
-</div>
-
-
-
-</div>
 
 
 </div>
